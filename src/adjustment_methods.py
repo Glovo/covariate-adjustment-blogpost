8b58d869e055f4ef5c2746153ec475151273d71e
David <david26694@gmail.com>
2022-12-02 16:00:36 +0100

    bjg

--- src/adjustment_methods.py
+++ src/adjustment_methods.py
@@ -1,9 +1,11 @@
-import statsmodels.formula.api as smf
 import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import statsmodels.formula.api as smf
 from sklearn.ensemble import HistGradientBoostingRegressor
 from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import cross_val_predict
-import numpy as np
+from sklearn.model_selection import KFold, cross_val_predict
+
 
 def get_ate_cuped(df, prediction_column="Y_before"):
     theta = df.cov().loc[prediction_column, "Y"] / df.cov().loc[prediction_column, "Y"]
@@ -34,16 +36,65 @@ def get_ate_cupac(df, pre_experiment_df, covariates=None):
 
 
 def get_ate_doubly_robust(data, covariates, propensity_score: float = 0.5):
+    data = data.copy().reset_index(drop=True)
     if not propensity_score:
         propensity_score = LogisticRegression(C=1e6, max_iter=1000).fit(data[covariates], data['T']).predict_proba(data[covariates])[:, 1]
     
     model_control = HistGradientBoostingRegressor()
-    outcome_regression_control = cross_val_predict(model_control, data.query('T==0')[covariates], data.query('T==0')['Y'], cv=2)
+    model_treatment = HistGradientBoostingRegressor()
+
+    predictions_control = pd.Series(index=data.index, dtype=float)
+    predictions_treatment = pd.Series(index=data.index, dtype=float)
+    for fold in KFold(n_splits=2, shuffle=True, random_state=42).split(data):
+        data_fold_0 = data.iloc[fold[0]]
+        data_fold_1 = data.iloc[fold[1]]
+        data_control_fold_0 = data_fold_0.query('T==0')
+        data_treatment_fold_0 = data_fold_0.query('T==1')
+        model_control.fit(
+            data_control_fold_0[covariates], data_control_fold_0['Y']
+        )
+        model_treatment.fit(
+            data_treatment_fold_0[covariates], data_treatment_fold_0['Y']
+        )
+
+        predictions_control[fold[1]] = model_control.predict(data_fold_1[covariates])
+        predictions_treatment[fold[1]] = model_treatment.predict(data_fold_1[covariates])
+
+    return (
+        np.mean(data['T']*(data['Y'] - predictions_treatment)/propensity_score + predictions_treatment) -
+        np.mean((1-data['T'])*(data['Y'] - predictions_control)/(1-propensity_score) + predictions_control)
+    )
+
+
+def get_ate_doubly_robust_wrong(data, covariates, propensity_score: float = 0.5):
+    if not propensity_score:
+        propensity_score = LogisticRegression(C=1e6, max_iter=1000).fit(data[covariates], data['T']).predict_proba(data[covariates])[:, 1]
+    
+    model_control = HistGradientBoostingRegressor()
+    outcome_regression_control = model_control.fit(
+        data.query('T==0')[covariates], data.query('T==0')['Y']
+    ).predict(data.query('T==0')[covariates])
 
     model_treatment = HistGradientBoostingRegressor()
-    outcome_regression_treatment = cross_val_predict(model_treatment, data.query('T==1')[covariates], data.query('T==1')['Y'], cv=2)
+    outcome_regression_treatment = model_treatment.fit(
+        data.query('T==1')[covariates], data.query('T==1')['Y']
+    ).predict(data.query('T==1')[covariates])
     
     return (
         np.mean(data.query('T==1')['T']*(data.query('T==1')['Y'] - outcome_regression_treatment)/propensity_score + outcome_regression_treatment) -
         np.mean((1-data.query('T==0')['T'])*(data.query('T==0')['Y'] - outcome_regression_control)/(1-propensity_score) + outcome_regression_control)
+    )
+
+
+def doubly_robust_matheus(data, covariates, ps: float =0.5):
+    T = 'T'
+    Y = 'Y'
+    if not ps:
+        ps = LogisticRegression(C=1e6, max_iter=1000).fit(data[covariates], data[T]).predict_proba(data[covariates])[:, 1]
+
+    mu0 = HistGradientBoostingRegressor().fit(data.query(f"{T}==0")[covariates], data.query(f"{T}==0")[Y]).predict(data[covariates])
+    mu1 = HistGradientBoostingRegressor().fit(data.query(f"{T}==1")[covariates], data.query(f"{T}==1")[Y]).predict(data[covariates])
+    return (
+        np.mean(data[T]*(data[Y] - mu1)/ps + mu1) -
+        np.mean((1-data[T])*(data[Y] - mu0)/(1-ps) + mu0)
     )
